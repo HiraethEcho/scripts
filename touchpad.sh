6397c0c9a563a16bf7c459a5e0b503b204ea6053
arch23 <wyz2016zxc@outlook.com>
2024-02-27 22:52:32 +0800

    update touchpad.sh using sed

--- touchpad.sh
+++ touchpad.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 
-id=$(xinput list | grep -i "Touchpad" | awk '{print $6}' | sed 's/[^0-9]//g')
+id=$(xinput list | grep -i "Touchpad" | sed -n -e 's/^.*id=/ /p' | cut -f1 )
 able=$(xinput list-props $id | grep "Device Enabled" | awk '{print $4}')
 if [[ "$able" == 1 ]]; then
   xinput disable $id
